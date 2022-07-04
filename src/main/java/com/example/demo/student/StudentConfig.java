940816ffc960476df9c6b2611e696356b42842c8
ermias_tegegn <ermiastegegn576@gmail.com>
2022-07-05 00:06:06 +0300

    commit student Config return student

--- src/main/java/com/example/demo/student/StudentConfig.java
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -1,14 +1,25 @@
 package com.example.demo.student;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
 
 @Configuration
 public class StudentConfig {
 
-    CommandLineRunner commandLineRunner(){
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
         return args -> {
-
+            new Student(
+                    1L,
+                    "Ermias",
+                    "ermias@gmail.com",
+                    LocalDate.of(2000, Month.FEBRUARY,6),
+                    234
+            );
         };
     }
 }
