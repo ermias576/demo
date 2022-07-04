900e11a6017fcef8729af81400107a6b93580ef7
ermias_tegegn <ermiastegegn576@gmail.com>
2022-07-05 00:15:08 +0300

    commit student Config save student

--- src/main/java/com/example/demo/student/StudentConfig.java
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.List;
 
 @Configuration
 public class StudentConfig {
@@ -13,13 +14,22 @@ public class StudentConfig {
     @Bean
     CommandLineRunner commandLineRunner(StudentRepository repository){
         return args -> {
-            new Student(
-                    1L,
+            Student ermias = new Student(
                     "Ermias",
                     "ermias@gmail.com",
-                    LocalDate.of(2000, Month.FEBRUARY,6),
+                    LocalDate.of(2000, Month.FEBRUARY, 6),
                     234
             );
+
+            Student mimi = new Student(
+                    "Mimi",
+                    "mimi@gmail.com",
+                    LocalDate.of(2000, Month.FEBRUARY, 6),
+                    234
+            );
+        repository.saveAll(
+                List.of(ermias, mimi)
+        );
         };
     }
 }
