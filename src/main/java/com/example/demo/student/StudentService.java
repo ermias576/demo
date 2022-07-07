7876ac3694e46aac3a66333e9247388247a6616f
ermias_tegegn <ermiastegegn576@gmail.com>
2022-07-07 20:04:03 +0300

    delete student

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -18,6 +18,15 @@ public class StudentService {
         this.studentRepository = studentRepository;
     }
 
+    public void deleteStudent(Long studentId) {
+
+        boolean exists = studentRepository.existsById(studentId);
+        if (!exists){
+            throw new IllegalStateException("student with id" + studentId + "does not exists");
+        }
+        studentRepository.deleteById(studentId);
+    }
+
     public List<Student> getStudents(){
         return studentRepository.findAll();
     }
